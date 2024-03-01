11d29a474e40b9dba4ea19fb20284c060f0288ef
Harshal Panse <112715180+Harshal9309@users.noreply.github.com>
2024-03-01 08:24:48 +0530

    Updated

--- paytm_app/serializers.py
+++ paytm_app/serializers.py
@@ -8,21 +8,21 @@ class UserProfileSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class InvestmentSerializer(serializers.ModelSerializer):
-    # user = UserProfileSerializer()
+    
     class Meta:
         model = Investment
         fields = '__all__'
 
 
 class ShoppingAssistantSerializer(serializers.ModelSerializer):
-    user = UserProfileSerializer()
+    
     class Meta:
         model = ShoppingAssistant
         fields = '__all__'
 
 
 class ChatboxSerializer(serializers.ModelSerializer):
-    user = UserProfileSerializer() 
+    
     class Meta:
         model = Chatbox
         fields = '__all__'
